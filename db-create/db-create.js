5b65b2ee4c872bb807de5ae4f635446e46e884ca
Peter <pld9bc@mail.missouri.edu>
2014-10-26 20:09:00 -0500

    tweet and user tables good
    everything except geo-located tweets should be working now....

--- db-create/db-create.js
+++ db-create/db-create.js
@@ -7,35 +7,34 @@
 var fs = require('fs');
 var Twit = require('twit');
 var T = new Twit({
-	  consumer_key:         // need to get your own credentials
-	, consumer_secret:      // need to get your own credentials
-	, access_token:         // need to get your own credentials
-	, access_token_secret:  // need to get your own credentials
+	  consumer_key: // get your own credentails
+	, consumer_secret: // get your own credentails
+	, access_token: // get your own credentails
+	, access_token_secret: // get your own credentails
 })
 var wstream = fs.createWriteStream('tweetDB.sql');
 
-wstream.write("DROP SCHEMA IF EXISTS tweets CASCADE;\n");	// init table
-wstream.write("CREATE SCHEMA tweets;\n");
-wstream.write("SET search_path = tweets;\n");
+wstream.write("DROP SCHEMA IF EXISTS twitStat CASCADE;\n");	// init table
+wstream.write("CREATE SCHEMA twitStat;\n");
+wstream.write("SET search_path = twitStat;\n");
 
 wstream.write("CREATE TABLE twit_user(\n\tusr_id varchar(50) primary key,\n\tname varchar(50),\n\tscreen_name varchar(50),\n\t");
 wstream.write("location varchar(50),\n\tdesription varchar(500),\n\tfollowers integer,\n\t");
-wstream.write("friends integer,\n\tcreated_at varchar(20),\n\tstatus_count integer,\n\t");
-wstream.write("fav_count integer,\n\tlang varchar(4),\n\tprofile_img_url varchar(300),\n\t");
+wstream.write("friends integer,\n\tcreated_at text,\n\tstatus_count integer,\n\t");
+wstream.write("fav_count integer,\n\tlang varchar(6),\n\tprofile_img_url varchar(300),\n\t");
 wstream.write("default_img boolean\n);\n");
 
-wstream.write("CREATE TABLE tweets(\n\tid serial primary key,\n\ttweet varchar(145) NOT NULL,\n\t");
-wstream.write("source varchar(300),\n\tuser_id integer\n);\n");
+wstream.write("CREATE TABLE tweets(\n\tid serial primary key,\n\ttweet text NOT NULL,\n\t");
+wstream.write("source varchar(300),\n\tuser_id bigint\n);\n");
 
 
-wstream.write("CREATE TABLE geo(\n\tid serial references tweets,\n\tgeo varchar(50),\n\t");
-wstream.write("coords varchar(50)\n);\n");
+wstream.write("CREATE TABLE geo(\n\tid serial references tweets,\n\tlat double precision,\n\tlong double precision\n);\n");
+//wstream.write("coords text\n);\n");
 
 wstream.write("");
 
 var stream = T.stream('statuses/sample')	// create stream object
 
-var count = 100;
 
 stream.on('tweet', function (tweet) {	// go and grab one tweet from stream
 //		console.log(tweet)	// log tweet onto screen
@@ -44,7 +43,11 @@ stream.on('tweet', function (tweet) {	// go and grab one tweet from stream
 		// create user entry
 		wstream.write("INSERT INTO twit_user VALUES (\n\t");
 		wstream.write("\'"+tweet.user.id+"\',\n\t");
-		wstream.write("\'"+tweet.user.name+"\',\n\t");
+		
+		var name = String(tweet.user.name);
+		var newname = name.replace(/'/g,"::");
+		newname = newname.replace(/"/g,";;");
+		wstream.write("\'"+newname+"\',\n\t");
 		wstream.write("\'"+tweet.user.screen_name+"\',\n\t");
 
 		var location = String(tweet.user.location);							// need to use local variable with replace...
@@ -53,7 +56,7 @@ stream.on('tweet', function (tweet) {	// go and grab one tweet from stream
 
 		var dsp = String(tweet.user.description);
 		var newdsp = dsp.replace(/'/g,";;")
-		console.log(newdsp);
+//		console.log(newdsp);
 		wstream.write("\'"+newdsp+"\',\n\t");
 		wstream.write( tweet.user.followers_count+",");
 		wstream.write( tweet.user.friends_count+",");
@@ -78,12 +81,18 @@ stream.on('tweet', function (tweet) {	// go and grab one tweet from stream
 		
 		wstream.write(tweet.user.id+"\n");
 		wstream.write(");\n");
-		
-		if(tweet.geo != null){
+/*		
+//		console.log(tweet.type);
+//		console.log(tweet.coordinates);
+		var geo = tweet.geo;
+
+		var geoStr = String(tweet.geo);
+		console.log(JSON.stringify(tweet.geo));
+		if(geoStr.indexOf("null")==-1){
 			wstream.write("INSERT INTO geo VALUES (\n\t");		
 			wstream.write("DEFAULT,\n\t");				
-			wstream.write("\'"+tweet.geo+"\',\n\t");
-			wstream.write("\'"+tweet.coordinates+"\'\n");
+//			wstream.write("\'"+geoStr+"\',\n\t");
+			wstream.write("\'"+JSON.stringify(tweet.geo)+"\'\n");
 			wstream.write(");\n");						
-		}
+		}*/
 })
